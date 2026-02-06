7505031699566a40b9c375d6c322dc951923181d
yokavad <msheikyokavad@gmail.com>
2026-02-06 20:50:25 +0100

    debug

---
+++ koko.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5 ,b =10 ,c ;
+       c = a / b;    
+    printf("The value of a is: %d\n", a);
+    printf("The value of b is: %d\n", b);
+    printf("The value of c is: %d\n", c);
+    return 0;
+}
