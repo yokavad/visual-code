3bbb3e8947ea34b65121600d04fdea0ba5d1db0a
yokavad <msheikyokavad@gmail.com>
2026-02-06 21:01:49 +0100

    ajouter d =a +b

--- koko.c
+++ koko.c
@@ -2,9 +2,11 @@
 
 int main() {
     int a = 5 ,b =10 ,c ,d ;
-       c = a / b;    
+       c = a / b;  
+       d=a+b; 
     printf("The value of a is: %d\n", a);
     printf("The value of b is: %d\n", b);
     printf("The value of c is: %d\n", c);
+    printf("The value of d is: %d\n", d);
     return 0;
 }
