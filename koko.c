1697da6c7216f7b5f13643f05f85819c3e38721e
yokavad <msheikyokavad@gmail.com>
2026-02-06 20:53:48 +0100

    ajouter d

--- koko.c
+++ koko.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5 ,b =10 ,c ;
+    int a = 5 ,b =10 ,c ,d ;
        c = a / b;    
     printf("The value of a is: %d\n", a);
     printf("The value of b is: %d\n", b);
