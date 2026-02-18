550b4acb1caf2945f0e8c0b2de5ea9891d5339d3
yokavad <msheikyokavad@gmail.com>
2026-02-18 08:51:42 +0100

    Ajouter f

--- koko.c
+++ koko.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5 ,b =10 ,c ,d ,e;
+    int a = 5 ,b =10 ,c ,d ,e ,f;
        c = a / b;  
        d=a+b; 
        e = a * b;   
+       f= a - b;
     printf("The value of a is: %d\n", a);
     printf("The value of b is: %d\n", b);
     printf("The value of c is: %d\n", c);
     printf("The value of d is: %d\n", d);
+    printf("The value of e is: %d\n", e);
+    printf("The value of f is: %d\n", f);
     return 0;
 }
